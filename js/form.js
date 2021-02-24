340e3b7a6d462401bdd47437047449090465d305
cat3a <cat3a-88@mail.ru>
2021-02-24 15:40:29 +0300

    Организует "выдачу детей и взрослых из хранилища" :laughing:

--- js/form.js
+++ js/form.js
@@ -3,15 +3,32 @@ const formPopup = document.querySelector(".hotel-selection-modal");
 const formIn = formPopup.querySelector(".hotel-modal-in");
 const formOut = formPopup.querySelector(".hotel-modal-out");
 const formSubmit = formPopup.querySelector(".selection-submit");
+const formAdult = formPopup.querySelector(".adult");
+const formChildren = formPopup.querySelector(".children");
+
 let isStorageSupport = true;
-let storage = "";
+let storageAdult = "0";
+let storageChildren = "2";
 let count = 0;
+
+try {
+  storageAdult = localStorage.getItem("adult");
+  storageChildren = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 formLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   formPopup.classList.toggle("hotel-form-show");
   formPopup.classList.toggle("hotel-form-animate");
   formPopup.classList.remove("form-error");
+
+  if (storageAdult || storageChildren) {
+    formAdult.value = storageAdult;
+    formChildren.value = storageChildren;
+  }
+
   formIn.focus();
 });
 
@@ -23,6 +40,11 @@ formSubmit.addEventListener("click", function (evt) {
     }
     formPopup.classList.add("form-error");
     count++;
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adult", formAdult.value);
+      localStorage.setItem("children", formChildren.value);
+    }
   }
 });
 
