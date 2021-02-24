93b1f9fbff7f343db91c926dc597417bf6ce7a02
cat3a <cat3a-88@mail.ru>
2021-02-24 14:07:09 +0300

    Добавляет изменения в js

--- js/form.js
+++ js/form.js
@@ -1,18 +1,21 @@
 const formLink = document.querySelector(".hotel-search");
 const formPopup = document.querySelector(".hotel-selection-modal");
 let formIn = formPopup.querySelector(".hotel-modal-in");
-let formOut = formPopup.querySelector(".hotel-modal-out")
+let formOut = formPopup.querySelector(".hotel-modal-out");
+let formSubmit = formPopup.querySelector(".selection-submit");
 
 formLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   formPopup.classList.toggle("hotel-form-show");
   formPopup.classList.toggle("hotel-form-animate");
+  formPopup.classList.remove("form-error");
   formIn.focus();
 });
 
-formPopup.addEventListener("click", function (evt) {
+formSubmit.addEventListener("click", function (evt) {
   if (!formIn.value || !formOut.value) {
     evt.preventDefault();
-    alert("Не заполнены обязательные поля дата заезда и дата выезда.");
+    formPopup.classList.add("form-error");
+    alert("Поля формы дата заезда и выезда обязательны для заполнения!")
   }
 });
