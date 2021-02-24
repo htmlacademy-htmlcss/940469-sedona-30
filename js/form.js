8994dcb072fa9c4dfd3032b20bb157872b07a782
cat3a <cat3a-88@mail.ru>
2021-02-24 11:50:19 +0300

    Начинает оживлять интерфейс

--- js/form.js
+++ js/form.js
@@ -1,1 +1,18 @@
-<script></script>
+const formLink = document.querySelector(".hotel-search");
+const formPopup = document.querySelector(".hotel-selection-modal");
+let formIn = formPopup.querySelector(".hotel-modal-in");
+let formOut = formPopup.querySelector(".hotel-modal-out")
+
+formLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  formPopup.classList.toggle("hotel-form-show");
+  formPopup.classList.toggle("hotel-form-animate");
+  formIn.focus();
+});
+
+formPopup.addEventListener("click", function (evt) {
+  if (!formIn.value || !formOut.value) {
+    evt.preventDefault();
+    alert("Не заполнены обязательные поля дата заезда и дата выезда.");
+  }
+});
